96e2849afbd543f3fae0ef53b2283fa199f6d591
BILLIE <billie@ubuntu.(none)>
2014-10-08 11:16:15 +0800

    updated db

--- fineadsdb.sql
+++ fineadsdb.sql
@@ -55,7 +55,7 @@ CREATE TABLE `messages` (
   `date_created` date DEFAULT NULL,
   `viewed` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`message_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,8 +64,32 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (1,'tres','123@gmail.com','haha','2014-10-07',1),(10,'test','haha@gago.com','12313testingmessage','2014-10-08',1),(11,'test','haha@gago.com','12313testingmessage','2014-10-08',1),(12,'tetete','123@t.com','hahaha','2014-10-08',1);
+INSERT INTO `messages` VALUES (1,'tres','123@gmail.com','haha','2014-10-07',1),(10,'test','haha@gago.com','12313testingmessage','2014-10-08',1),(11,'test','haha@gago.com','12313testingmessage','2014-10-08',1),(12,'tetete','123@t.com','hahaha','2014-10-08',1),(13,'testqq','43@TY.COM','TRY','2014-10-08',1);
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `portfolio`
+--
+
+DROP TABLE IF EXISTS `portfolio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `portfolio` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+LOCK TABLES `portfolio` WRITE;
+/*!40000 ALTER TABLE `portfolio` DISABLE KEYS */;
+INSERT INTO `portfolio` VALUES (3,'animation.gif-phpVDmhjH'),(4,'frontview.JPG-phpZpftMy');
+/*!40000 ALTER TABLE `portfolio` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -77,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-10-08  8:51:18
+-- Dump completed on 2014-10-08 11:15:35
