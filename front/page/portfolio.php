1f51e32d505c919f75b1cb7d56d8159b632dd1d8
BILLIE <billie@ubuntu.(none)>
2014-10-10 13:20:26 +0800

    upload fix

--- front/page/portfolio.php
+++ front/page/portfolio.php
@@ -34,11 +34,11 @@ class Front_Page_Portfolio extends Front_Page {
 		if (isset($_FILES['image'])){
 			move_uploaded_file(
 				$_FILES['image']['tmp_name'],
-				dirname(__FILE__).'/../../web/images/'.$_FILES['image']['name']."-".str_replace('/tmp/',"",($_FILES['image']['tmp_name'])));
+				dirname(__FILE__).'/../../web/images/'.$_FILES['image']['name']);
 			front()
 				->portfolio()
 				->create(
-					$_FILES['image']['name']."-".str_replace('/tmp/',"",($_FILES['image']['tmp_name'])));
+					$_FILES['image']['name']);
 			return "true";
 		}
 		return $this->_page();
